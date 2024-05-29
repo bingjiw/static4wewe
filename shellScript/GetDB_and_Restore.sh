3516fdf68d7726158eed49eee6f61e92f3288092
bingjiw <391904577@qq.com>
2024-05-30 07:46:20 +0800

    Update GetDB_and_Restore.sh

--- shellScript/GetDB_and_Restore.sh
+++ shellScript/GetDB_and_Restore.sh
@@ -1,1 +1,13 @@
-
+echo "容器重启动后one-api.db文件还未创建，应不存在，获取SQLite DB文件，以恢复原数据"
+if [ ! -f /data/one-api.db ]; then
+    wget -O /data/Encrypted_Compressed_SQLiteDB.zip "https://raw.githubusercontent.com/bingjiw/static_key.wewegpt.com/main/SQLite_DB_file/Encrypted_Compressed_SQLiteDB.zip"
+    echo "Encrypted_Compressed_SQLiteDB.zip文件从Github下载成功"
+    # 安装解压缩工具
+    apk add unzip
+    # 解压缩文件，使用从参数 $1 传入的密码
+    unzip -P "$1" /data/Encrypted_Compressed_SQLiteDB.zip -d /data/
+    echo "Encrypted_Compressed_SQLiteDB.zip文件解压缩成功，one-api启动时应可以发现one-api.db数据库文件并使用它加载原有数据。"
+else
+    echo "one-api.db 文件已经存在，无需下载"
+fi
+echo "--------------------------------"
