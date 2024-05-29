5f728c72eaae3f169084dc615f487572e68f0f55
bingjiw <391904577@qq.com>
2024-05-30 07:57:32 +0800

    Update GetDB_and_Restore.sh

--- shellScript/GetDB_and_Restore.sh
+++ shellScript/GetDB_and_Restore.sh
@@ -5,9 +5,15 @@ if [ ! -f /data/one-api.db ]; then
     echo "Encrypted_Compressed_SQLiteDB.zip文件从Github下载成功"
     # 安装解压缩工具
     apk add unzip
+
     # 解压缩文件，使用从参数 $1 传入的密码
-    unzip -P "$1" /data/Encrypted_Compressed_SQLiteDB.zip -d /data/
-    echo "Encrypted_Compressed_SQLiteDB.zip文件解压缩成功，one-api启动时应可以发现one-api.db数据库文件并使用它加载原有数据。"
+    if unzip -P "$1" /data/Encrypted_Compressed_SQLiteDB.zip -d /data/; then
+        echo "Encrypted_Compressed_SQLiteDB.zip文件解压缩成功，one-api启动时应可以发现one-api.db数据库文件并使用它加载原有数据。"
+    else
+        echo "Encrypted_Compressed_SQLiteDB.zip文件解压缩失败!!!(可能是由于传入的密码错误)"
+        exit 1
+    fi
+    
 else
     echo "one-api.db 文件已经存在，无需下载"
 fi
