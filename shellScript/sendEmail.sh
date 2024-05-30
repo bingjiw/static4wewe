be538aba3519a3b7298f08a71b55cfa6c82d210f
bingjiw <391904577@qq.com>
2024-05-31 00:41:16 +0800

    Update sendEmail.sh

--- shellScript/sendEmail.sh
+++ shellScript/sendEmail.sh
@@ -67,8 +67,8 @@ fi
 echo "#VVVVVVVV 数据库备份情况每日报告 VVVVVVVV"
 # 获取日期
 today=$(date +%Y-%m-%d)
-yesterday=$(date -d "$(date +%Y-%m-%d) - 1 day" +%Y-%m-%d)
-day_before_yesterday=$(date -d "$(date +%Y-%m-%d) - 2 days" +%Y-%m-%d)
+yesterday=$(date -d "@$(( $(date +%s) - 86400 ))" +%Y-%m-%d)
+day_before_yesterday=$(date -d "@$(( $(date +%s) - 172800 ))" +%Y-%m-%d)
 
 # 文件名
 TodayLogFilename="DailyReport-$today.log"
