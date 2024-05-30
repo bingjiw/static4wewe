fce7f04edbecb485c62c9449e3df05cd3c83258c
bingjiw <391904577@qq.com>
2024-05-30 16:03:13 +0800

    Update BackupDB_and_SetNext_at.sh

--- shellScript/BackupDB_and_SetNext_at.sh
+++ shellScript/BackupDB_and_SetNext_at.sh
@@ -18,11 +18,11 @@ else
     echo "atd服务已启动"
 fi
 
-echo "# 将BackupDB.sh脚本设置为可执行"
-chmod +x /data/BackupDB.sh
-
 echo "# 设定20分钟后执行BackupDB_and_SetNext_at.sh脚本"
 echo "/data/BackupDB_and_SetNext_at.sh" | at now + 20 minutes
+
+####echo "# 将BackupDB.sh脚本设置为可执行"
+####已经在设于RUN Commands的命令中执行过了： chmod +x /data/BackupDB.sh
 
 echo "# 以最低CPU优先级执行BackupDB.sh"
 nice -n 19 /data/BackupDB.sh
