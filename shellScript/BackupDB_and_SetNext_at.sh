f5d74c3c409cc5e9be4c4b5052aee24345feeb43
bingjiw <391904577@qq.com>
2024-05-30 12:41:31 +0800

    Update BackupDB_and_SetNext_at.sh

--- shellScript/BackupDB_and_SetNext_at.sh
+++ shellScript/BackupDB_and_SetNext_at.sh
@@ -21,9 +21,8 @@ fi
 echo "# 将BackupDB.sh脚本设置为可执行"
 chmod +x /data/BackupDB.sh
 
-echo "# 以最低CPU优先级执行BackupDB.sh"
-nice -n 19 /data/BackupDB.sh
-
 echo "# 设定20分钟后执行BackupDB_and_SetNext_at.sh脚本"
 echo "/data/BackupDB_and_SetNext_at.sh" | at now + 20 minutes
 
+echo "# 以最低CPU优先级执行BackupDB.sh"
+nice -n 19 /data/BackupDB.sh
