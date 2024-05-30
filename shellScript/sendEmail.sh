33649e328942204e650c76f5b11c8bcec8801501
bingjiw <391904577@qq.com>
2024-05-30 19:05:20 +0800

    Update sendEmail.sh

--- shellScript/sendEmail.sh
+++ shellScript/sendEmail.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# 记录脚本的开始时间
+echo "#--执行 sendEmail.sh--"
+
+echo "# 记录脚本的开始时间"
 start_time=$(date +%s)
 
-if [ -z "$1" ]; then
-    echo "未传入参数：SMTP发信密码。无法发信。"
+if [ -z "$SMTP_PASSWORD" ]; then
+    echo "无环境变量 SMTP_PASSWORD（SMTP发信密码）无法发信。"
     exit 1
 fi
-# 获取传递的环境变量 SMTP发信密码，执行本脚本时需带参数，如./script2.sh 'secretWord'
-SMTP_PASSWORD="$1"
-echo "收到入传的参数（用作SMTP发信密码）：$1"
 
 echo "安装：发带附件的邮件 的工具"
 echo "若mutt没有安装，就安装mutt。若msmtp没有安装，就安装msmtp。"
 
-# 检查并安装 mutt
+echo "# 检查并安装 mutt"
 if ! command -v mutt >/dev/null 2>&1; then
     echo "mutt 未安装，正在安装..."
     apk add mutt
@@ -22,7 +21,7 @@ else
     echo "mutt 已安装"
 fi
 
-# 检查并安装 msmtp
+echo "# 检查并安装 msmtp"
 if ! command -v msmtp >/dev/null 2>&1; then
     echo "msmtp 未安装，正在安装..."
     apk add msmtp
@@ -30,7 +29,7 @@ else
     echo "msmtp 已安装"
 fi
 
-echo "# 配置 msmtp"
+echo "# 配置 msmtp, 写/root/.msmtprc"
 cat > /root/.msmtprc <<EOL
 defaults
 auth           on
@@ -49,7 +48,7 @@ EOL
 echo "# 确保 msmtprc 文件权限"
 chmod 600 /root/.msmtprc
 
-echo "# 配置 mutt 使用 msmtp 发送邮件"
+echo "# 配置 mutt，写/root/.muttrc，以使用 msmtp 发送邮件"
 cat > /root/.muttrc <<EOL
 set sendmail="/usr/bin/msmtp"
 set use_from=yes
@@ -58,12 +57,12 @@ set from=xiaorong.boy@icloud.com
 set envelope_from=yes
 EOL
 
-#发邮件
+echo "#发邮件"
 echo -e "Send on: $(date) \n\n by key1api-web app in container. \n\n The DB file is compressed and encrypted." | mutt -s "one-api.db for backup" -a /data/Encrypted_Compressed_SQLiteDB.zip -- LLC.Good.House@gmail.com
 
-# 记录结束时间
+echo "# 记录结束时间"
 end_time=$(date +%s)
 
-# 计算并显示耗时
+echo "# 计算并显示这一次 sendEmail 的耗时"
 elapsed_time=$((end_time - start_time))
-echo "sendEmail.sh脚本执行共耗时：$elapsed_time 秒"
+echo "sendEmail.sh 脚本执行共耗时：$elapsed_time 秒"
